c992d7fa92df277ccd3a58864266310f3fe296c1
Amandubey12 <76807303+Amandubey12@users.noreply.github.com>
2021-10-02 21:18:38 +0530

    first commit

---
+++ js/hidepicture.js
@@ -0,0 +1,8 @@
+const myFunction = () => {
+    document.getElementById("first").style.display ='block';
+    document.getElementById("second").style.display ='none';
+          }
+          const myFunction2 = () => {
+    document.getElementById("second").style.display ='block'
+    document.getElementById("first").style.display ='none';
+          }
